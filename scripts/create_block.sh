881f5d318e24b463c534faffdfa9bb43b459d4ab
Jeff Fischer <jeffrey.fischer@genforma.com>
2011-12-09 15:59:32 -0800

    Enhanced create_block.sh to create drivers as well; added drivers

--- scripts/create_block.sh
+++ scripts/create_block.sh
@@ -1,4 +1,57 @@
-BDIR=blox/$1__1_0
-mkdir $BDIR
-touch $BDIR/__init__.py
-touch $BDIR/e_$1.py
+#!/bin/bash
+
+# Creates the skeleton for a block as well as its engage driver.
+# Takes the blockname as an argument. The blockname should be exactly
+# as used in the configuration file, including matching case. Otherwise,
+# the associated driver won't be found by the loader. For example, use
+# File-Mongo instead of file-mongo or file_mongo. The directory name will be
+# adjusted to have all lower case and underscores.
+
+
+if [[ "$BLOXPATH" == "" ]]; then
+  BLOXPATH=`pwd`/blox
+fi
+if ! [ -d $BLOXPATH ]; then
+  echo "Unable to find blox directory at $BLOXPATH"
+  exit 1
+fi
+
+ENGAGE=`cd $BLOXPATH/../engage; pwd`
+if ! [ -d $ENGAGE ]; then
+  echo "Unable to find engage directory at $ENGAGE"
+  exit 1
+fi
+
+
+if [[ "$#" != "1" ]]; then
+  echo "create_block.sh <bockname>"
+  exit 1
+fi
+
+# The block module name name should be all lower case and a valid python package
+BLOCK=`echo $1 | tr '[:upper:]' '[:lower:]' | tr '[-.]' '[__]'`
+BLOCKNAME=$1
+
+BDIR=$BLOXPATH/${BLOCK}__1_0
+if [ -d $BDIR ]; then
+  echo "Skipping creation of " $BDIR " - it already exists."
+else
+  mkdir $BDIR
+  touch $BDIR/__init__.py
+  touch $BDIR/e_$BLOCK.py
+fi
+
+DRIVERDIR=$ENGAGE/datablox/drivers/${BLOCK}__1_0
+if [ -d $DRIVERDIR ]; then
+  echo "Nothing to do: driver directly $DRIVERDIR already exists"
+else
+  mkdir $DRIVERDIR
+  touch $DRIVERDIR/__init__.py
+  cp $ENGAGE/block_driver_template.py $DRIVERDIR/driver.py
+  sed "s/BLOCKNAME/$BLOCKNAME/g" <$ENGAGE/block_resource.json.tmpl | sed "s/BLOCKDIR/${BLOCK}__1_0/g" >$DRIVERDIR/resources.json
+  sed "s/BLOCKNAME/$BLOCKNAME/g" <$ENGAGE/block_packages.json.tmpl | sed "s/BLOCKDIR/${BLOCK}__1_0/g" >$DRIVERDIR/packages.json
+  echo "Created engage driver at $DRIVERDIR"
+fi
+
+echo "Block creation for $BLOCK successful."
+exit 0
