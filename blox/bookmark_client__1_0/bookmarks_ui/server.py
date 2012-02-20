183b64fc7c7c9b050c68e7dba2b4737766249796
Sai Deep Tetali <t.saideep@gmail.com>
2012-02-20 10:00:05 -0800

    Bookmarks UI added to bookmark-client block
    This may be moved.

---
+++ blox/bookmark_client__1_0/bookmarks_ui/server.py
@@ -0,0 +1,11 @@
+import CGIHTTPServer
+import BaseHTTPServer
+
+class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
+    cgi_directories = ["/"]
+
+PORT = 9000
+
+httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+print "serving at port", PORT
+httpd.serve_forever()
