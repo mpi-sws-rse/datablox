6aab15800d019667cf51199aa940afea325641b7
saideep <saideep@vrt-55-14.(none)>
2012-03-03 00:55:39 +0100

    Fix for detecting local ip-address

--- datablox_framework/datablox_framework/block.py
+++ datablox_framework/datablox_framework/block.py
@@ -102,7 +102,7 @@ class BlockUtils(object):
     if node_ipaddress == None:
       ip = socket.gethostbyname(socket.gethostname())
       #that hasn't worked, using a method described here: http://zeth.net/post/355/
-      if ip == "127.0.0.1":
+      if ip[:4] == "127.":
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('google.com', 0))
         ip = s.getsockname()[0]
