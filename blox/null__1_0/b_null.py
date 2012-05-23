8b052eb70c7a4fcadfaa05594d3f602062267a55
Jeff Fischer <jeffrey.fischer@genforma.com>
2012-05-22 20:43:06 -0700

    Add engage driver for null block; print more stats

--- blox/null__1_0/b_null.py
+++ blox/null__1_0/b_null.py
@@ -7,11 +7,13 @@ class null(Block):
     self.add_port("input", Port.PUSH, Port.UNNAMED, [])
     self.sleep_time = config["sleep"] if config.has_key("sleep") else 0
     self.logs = 0
+    self.rows = 0
     self.log(INFO, "NULL block loaded")
 
   def recv_push(self, port, log):
     self.logs += 1
+    self.rows += log.num_rows()
     time.sleep(self.sleep_time)
   
   def on_shutdown(self):
-    self.log(INFO, "got %d logs" % self.logs)
+    self.log(INFO, "got %d logs, %d rows" % (self.logs, self.rows))
