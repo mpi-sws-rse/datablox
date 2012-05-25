e7632d4553c902c285a26cd9d3b273b02be76a36
Jeff Fischer <jeffrey.fischer@genforma.com>
2012-05-25 10:51:25 -0700

    Add testing

---
+++ scripts/check_solr_results.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+search_term=$1
+expected_count=$2
+matches=`curl -s "http://127.0.0.1:8983/solr/select/?q=${search_term}&version=2.2&start=0&rows=10&indent=on" | grep numFound | awk '{print $3}' | python -c "import re;import sys; print re.search(r'\d+', sys.stdin.read()).group(0)"`
+if [[ "$matches" == "$expected_count" ]]; then
+  echo "Got correct number of search hits"
+  exit 0
+else:
+  echo "Got wrong number of search hits: expected $expected_count, got $matches"
+  exit 1
+fi
