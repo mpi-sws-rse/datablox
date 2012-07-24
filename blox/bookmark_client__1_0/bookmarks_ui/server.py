34b95921a1f2f2794f3fb1e3b4a3dc979a2a039b
Jeff Fischer <jeffrey.fischer@genforma.com>
2012-07-24 12:37:37 -0700

    Add test-bookmarks; still have an open issue with issue #78

--- blox/bookmark_client__1_0/bookmarks_ui/server.py
+++ blox/bookmark_client__1_0/bookmarks_ui/server.py
@@ -2,12 +2,25 @@
 
 import CGIHTTPServer
 import BaseHTTPServer
+from optparse import OptionParser
+import os
+import os.path
 
 class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
     cgi_directories = ["/www"]
 
 PORT = 9000
 
+parser = OptionParser()
+parser.add_option("--pidfile", dest="pidfile", default=None,
+                  help="write out pid to the specified file")
+(options, args) = parser.parse_args()
+        
 httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
 print "serving at port", PORT
+if options.pidfile:
+    pidfilename = os.path.abspath(os.path.expanduser(options.pidfile))
+    with open(pidfilename, "w") as f:
+        f.write("%d" % os.getpid())
+        print "wrote pid %d to %s" % (os.getpid(), pidfilename)
 httpd.serve_forever()
