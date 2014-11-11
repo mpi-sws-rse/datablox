3a8dcb576718ebda4c85c8c138723707350ee62c
Jeff Fischer <jeffrey.fischer@gmail.com>
2014-11-11 13:19:04 -0800

    Changes for benchmark

--- blox/null__1_0/b_null.py
+++ blox/null__1_0/b_null.py
@@ -5,7 +5,7 @@ from logging import ERROR, WARN, INFO, DEBUG
 class null(Block):
   def on_load(self, config):
     self.add_port("input", Port.PUSH, Port.UNNAMED, [])
-    self.sleep_time = config["sleep"] if config.has_key("sleep") else 0
+    self.sleep_time = config["sleep"] if config.has_key("sleep") else None
     self.logs = 0
     self.rows = 0
     self.log(INFO, "NULL block loaded")
@@ -13,7 +13,8 @@ class null(Block):
   def recv_push(self, port, log):
     self.logs += 1
     self.rows += log.num_rows()
-    time.sleep(self.sleep_time)
+    if self.sleep_time is not None:
+      time.sleep(self.sleep_time)
   
   def on_shutdown(self):
     self.log(INFO, "got %d logs, %d rows" % (self.logs, self.rows))
